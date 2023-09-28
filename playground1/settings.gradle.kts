eaaa7ac991038a656d908a5291bc17ea9cc05684
Sunsh4rd <SsoslickK@gmail.com>
2023-09-28 17:13:35 +0400

    subplayground

--- playground1/settings.gradle.kts
+++ playground1/settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "playground1"
+include("subplayground")
--- playground1/src/main/java/ru/sccs/playground1/Playground1Application.java
+++ playground1/src/main/java/ru/sccs/playground1/Playground1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Playground1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Playground1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Playground1Application.class, args);
+    }
 
 }
